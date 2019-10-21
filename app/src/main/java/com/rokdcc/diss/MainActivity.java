cd1238bb59a606425e338d74fb92f87472740e51
ByongRyunShin <nj298@kumoh.ac.kr>
2019-10-22 01:20:49 +0900

    Add Bluetooth Force ON Function

--- app/src/main/java/com/rokdcc/diss/MainActivity.java
+++ app/src/main/java/com/rokdcc/diss/MainActivity.java
@@ -2,7 +2,9 @@ package com.rokdcc.diss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(mBluetoothAdapter == null){
+            Log.i("DEBUG_TAG", "NO Bluetooth Error");
+        }
+        else{
+            if(!mBluetoothAdapter.isEnabled()){
+                mBluetoothAdapter.enable();
+                Log.i("DEBUG_TAG","Bluetooth ON");
+            }
+        }
     }
 }
