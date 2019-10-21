e5717cf622c5034181bd5798a81a3ed027784c1f
ByongRyunShin <nj298@kumoh.ac.kr>
2019-10-22 00:29:30 +0900

    Add Splash Activity

---
+++ app/src/main/java/com/rokdcc/diss/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.rokdcc.diss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //상태바 제거 코드
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 3000); // 1초 후에 hd handler 실행  3000ms = 3초
+    }
+    private class splashhandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class)); //로딩이 끝난 후, ChoiceFunction 이동
+            SplashActivity.this.finish(); // 로딩페이지 Activity stack에서 제거
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+
+}
