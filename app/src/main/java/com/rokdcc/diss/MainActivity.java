db549e518c7879b4ab317d6962da586f8e31c0dc
ByongRyunShin <nj298@kumoh.ac.kr>
2019-10-22 21:08:53 +0900

    Add Beacon Scanning Function

--- app/src/main/java/com/rokdcc/diss/MainActivity.java
+++ app/src/main/java/com/rokdcc/diss/MainActivity.java
@@ -1,29 +1,206 @@
 package com.rokdcc.diss;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
 
-    @Override
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+    protected static final String TAG = "BeaconActivity";
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private BeaconManager beaconManager;
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if(mBluetoothAdapter == null){
+        if (mBluetoothAdapter == null) {
             Log.i("DEBUG_TAG", "NO Bluetooth Error");
-        }
-        else{
-            if(!mBluetoothAdapter.isEnabled()){
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
                 mBluetoothAdapter.enable();
-                Log.i("DEBUG_TAG","Bluetooth ON");
+                Log.i("DEBUG_TAG", "Bluetooth ON");
+            }
+        }
+         verifyBluetooth();
+
+         //Bluetooth Permission
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+             if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                 final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                 builder.setTitle("This app needs location access");
+                 builder.setMessage("Please grant location access so this app can detect beacons in the background.");
+                 builder.setPositiveButton(android.R.string.ok, null);
+                 builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                     @TargetApi(23)
+                     @Override
+                     public void onDismiss(DialogInterface dialog) {
+                         requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                 PERMISSION_REQUEST_COARSE_LOCATION);
+                     }
+
+                 });
+                 builder.show();
+             }
+         }
+
+         beaconManager = BeaconManager.getInstanceForApplication(this);
+         beaconManager.bind(this);
+    }
+    // Checking needed permissions
+    private void verifyBluetooth() {
+        try {
+            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Bluetooth not enabled");
+                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                builder.show();
+            }
+        }
+        catch (RuntimeException e) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Bluetooth LE not available");
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                    System.exit(0);
+                }
+            });
+            builder.show();
+        }
+    }
+
+    // Checking needed permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+                    });
+                    builder.show();
+                }
+                return;
             }
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (beaconManager.isBound(this))
+            beaconManager.setBackgroundMode(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (beaconManager.isBound(this))
+            beaconManager.setBackgroundMode(false);
+    }
+
+    @Override
+    // Called when the beacon service is running and ready to accept your commands through the BeaconManager
+    public void onBeaconServiceConnect() {
+        // addRangeNotifier specifies a class that should be called each time the BeaconService gets ranging data, which is nominally once per second when beacons are detected.
+        // RangeNotifier interface is implemented by classes that receive beacon ranging notifications
+        beaconManager.removeAllMonitorNotifiers();
+        beaconManager.addMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                Log.i(TAG, "I just saw an beacon for the first time!");
+                Toast.makeText(getApplicationContext(), "전투통제실 입장, 차단모드 실행", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                Log.i(TAG, "I no longer see an beacon");
+                Toast.makeText(getApplicationContext(), "전투통제실 퇴장, 차단모드 해제", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int state, Region region) {
+                Log.i(TAG, "I have just switched from seeing/not seeing beacons: "+state);
+            }
+        });
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitorUniqueId", null, null, null));
+        } catch (RemoteException e) {   }
+    }
+
+    public void showNotification(String title, String message){
+         Intent notifyIntent = new Intent(this, MainActivity.class);
+         notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+         PendingIntent pendingIntent = PendingIntent.getActivities(this, 0, new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT );
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+
+    }
+
 }
