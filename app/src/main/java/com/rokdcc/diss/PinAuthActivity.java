52c0620ed3c9e0143b74c9bcafd5c06a4ee4241f
ByongRyunShin <nj298@kumoh.ac.kr>
2019-10-23 20:22:08 +0900

    FirstAuthActivity Added

---
+++ app/src/main/java/com/rokdcc/diss/PinAuthActivity.java
@@ -0,0 +1,14 @@
+package com.rokdcc.diss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PinAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_auth);
+    }
+}
