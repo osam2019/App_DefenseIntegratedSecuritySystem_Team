6c10b2b324567525c17de07380984b8b22bc4c69
ByongRyunShin <nj298@kumoh.ac.kr>
2019-10-23 20:32:56 +0900

    add pin auth activity and auth -ing activity complete

---
+++ app/src/main/java/com/rokdcc/diss/AuthingActivity.java
@@ -0,0 +1,14 @@
+package com.rokdcc.diss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authing);
+    }
+}
